bf2f6c542d50a82f58601d31216d5b63d4602f84
Dmitry Badichan <d.badichan@youngcapital.nl>
2022-03-24 10:26:06 +0200

    lesson2.3

--- lesson2.1.tsx
+++ lesson2.1.tsx
@@ -13,14 +13,62 @@ const fetchResume = (data = {}) => {
   return fetch(`http://google.com/api/v1/resumes`, data)
 }
 
+/*
+/index.tsx
+
+/store/index.ts
+
+/reducers/
+  jobs.ts
+  resumes.ts
+  users.ts
+
+/helpers
+  formaMoney.ts
+  userName.ts
+
+/pages
+  main.ts
+  resumes.ts
+  vacancies.ts
+
+/components
+1.
+  Footer
+  Header
+  VacancySearchInput.ts
+  ResumeSearchInput.ts
+2.
+  /ui
+    Footer
+    Header
+    Sidebar
+  /vacancies
+    SearchINput
+    SortSelect
+    TagsSelect
+  /resumes
+    SearchInput
+    SortSelect
+    TagsSelect
+    SalaryInput
+
+*/
+
 // resumes.ts
+export const SET_RESUME = 'SET_RESUME';
+
 const initialState= {
   list: []
 }
 
-const resumes = (state = initialState, action) => {
+type State = {
+  list: Resume[]
+}
+
+const resumes = (state: State = initialState, action: Action) => {
   switch (action.type) {
-    case 'SET_RESUME':
+    case SET_RESUME:
         return {
           list: action.list
         }
@@ -32,47 +80,69 @@ const resumes = (state = initialState, action) => {
   }
 }
 
-const useResumes = () => {
-  const [search, setSearch] = React.useState({
+type Resume = {
+  id: number,
+  name: string
+}
+
+type setResumeAction = {
+ type: typeof  SET_RESUME,
+ list: Resume[]
+}
+
+type Action = setResumeAction
+
+const setResumes = (body: Resume[]): Action => {
+  return {
+    type: SET_RESUME,
+    list: body
+  }
+}
+
+type SearchType = {
+  salary: number,
+  currency: string
+}
+
+type State = {
+  list: Resume[],
+  handleChangeCurrency(value: number): void,
+  handleChangeSalary(value: string): void
+}
+
+const useResumes = (): State => {
+  const [search, setSearch] = React.useState<SearchType>({
     salary: 0,
     currency: ''
   })
 
-  const { list } = useSelector(state=>state.resumes)
+  const { list } = useSelector<AppState, ResumeState>(state=>state.resumes)
 
   React.useEffect(() => {
-    fetchResume().then(data=>dispatch({
-      type: 'SET_RESUME',
-      list: data.body
-    }))
+    fetchResume().then(data=>dispatch(setResumes(data.body)))
   }, [])
 
-  const handleChangeSalary = (value) => {
+  const handleChangeSalary = (value: number) => {
     setSearch({
       ...search,
       salary: value
     })
-    fetchResume(search).then(data=>dispatch({
-      type: 'SET_RESUME',
-      list: data.body
-    }))
+    fetchResume(search).then(data=>dispatch(setResumes(data.body)))
   }
 
-  const handleChangeCurrency = (value) => {
+  const handleChangeCurrency = (value: string) => {
     setSearch({
       ...search,
       currency: value
     })
-    fetchResume(search).then(data=>dispatch({
-      type: 'SET_RESUME',
-      list: data.body
-    }))
+    fetchResume(search).then(data=>dispatch(setResumes(data.body)))
   }
 
   return {
+    handleChangeSalary,
+    handleChangeCurrency,
     list
   }
-
 }
 
 const App = () => {
@@ -84,14 +154,14 @@ const App = () => {
       handleChangeSalary(e.target.value)
     }} />
 
-    <select onChange={(e) => {
+    <select onChange={(e: InputEvent) => {
       handleChangeCurrency(e.target.value)
     }} >
       <option value="euro">Euro</option>
       <option value="rub">Rub</option>
     </select>
 
-    <select onChange={(e) => {
+    <select onChange={(e: SelectEvent) => {
     }} >
       <option value="php">PHP</option>
       <option value="react">React</option>
@@ -100,7 +170,6 @@ const App = () => {
     {
       data.map(item=><p>{item.name}</p>)
     }
-
 
     Footer
   </div>
