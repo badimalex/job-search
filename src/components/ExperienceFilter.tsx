1a4a2832ca521aabb67ca04518fb4571d8a0af16
Dmitry Badichan <d.badichan@youngcapital.nl>
2022-04-16 20:11:31 +0300

    Fix

--- src/components/ExperienceFilter.tsx
+++ src/components/ExperienceFilter.tsx
@@ -5,11 +5,9 @@ import TextField from '@mui/material/TextField';
 export default function BasicTextFields() {
 
   return (
-    <Box component="form"
-      sx={{
+    <Box component="form" sx={{
         '& > :not(style)': { m: 1, width: '25ch' },
-      }}
-      noValidate
+      }} noValidate
       autoComplete="off"
     >
       <TextField id="outlined-basic" label="Outlined" variant="outlined" />
